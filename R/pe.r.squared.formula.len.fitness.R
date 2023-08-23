1d7af4eb480ca6ad00558689753dd3a63e9a69b0
Danilo Tomasoni <tomasoni@cosbi.eu>
2023-08-23 11:03:28 +0200

    conversion

--- R/pe.r.squared.formula.len.fitness.R
+++ R/pe.r.squared.formula.len.fitness.R
@@ -24,7 +24,7 @@
 #'}
 pe.r.squared.formula.len.fitness <- function(errs.m, max.formula.len){
   x0 <- 0.4
-  denominator <- exp(10*errs.m$formula.len/max.formula.len*errs.m$base.pe)
+  denominator <- exp(10*as.integer(errs.m$formula.len)/max.formula.len*errs.m$base.pe)
   numerator <- sign(errs.m$base.r.squared)*(errs.m$base.r.squared/x0)^2
   numerator / denominator
 }

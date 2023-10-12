9ca432fae24e81d6c681e30831a28d8d2b4f3922
Danilo Tomasoni <tomasoni@cosbi.eu>
2023-10-12 10:29:45 +0200

    exposed compute.regressors and normalize for manual inspection and generation of formula beta and p-values.

--- R/compute.regressors.R
+++ R/compute.regressors.R
@@ -1,3 +1,41 @@
+#' Compute regressors of a given non-linear formula
+#'
+#'
+#' @seealso random.search
+#' @seealso genetic.search
+#'
+#' @param base.X.df.std A data.frame with the base variables that compose the terms of the non-linear formula
+#' @param parsed.vars The results of the symbolicr::parsed.vars
+#' @param transformations A list of potentially non-linear transformations that can be used in the formula terms.
+#' @param X.mean.sd The `mean.sd` list entry of the returned value of `normalize`
+#' @param regressors.min.values The output of `dataset.min.maxs`. If NULL, minimum values are computed on `base.X.df.std`. Use this parameter if you want to fix the minimum values, for instance if you compute it in a subset of the full dataset, as happens in the cross-validation.
+#'
+#' @return Return a data.frame with the columns corresponding to each formula term.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' best.vars <- c('inv.mul.patch_hyd_5.patch_hyd_5','mul.patch_ion_3.patch_pos','patch_pos_3')
+#'
+#' # parse variables
+#' parsed.vars <- symbolicr::parse.vars(best.vars, regressors, transformations)
+#' # standardize
+#' norm.res <- symbolicr::normalize(regressors.df, custom.mins=list())
+#' regressors.df.std <- norm.res$X.std
+#' regressors.mean.sd <-norm.res$mean.sd
+#' # compute regressors
+#' regressors.def <- symbolicr::compute.regressors(regressors.df.std, parsed.vars,
+#'                    transformations, regressors.mean.sd, regressors.min.values=NULL)
+#' regressors.min.values <- regressors.def$min.values
+#' formula.df <- regressors.def$regressors
+#'
+#' # extract coefficients of given formula
+#' dataset.std <- cbind(formula.df, y)
+#' cur.formula.str <- paste0('y',"~",paste(best.vars, collapse=' + '))
+#' base.lm <- lm(as.formula(base.formula), data=dataset.std)
+#' base.lm$coefficients
+#' }
+#'
 compute.regressors <- function(base.X.df.std, parsed.vars, transformations, X.mean.sd, regressors.min.values=NULL){
   regressors <- names(base.X.df.std)
 
